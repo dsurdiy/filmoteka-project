e25d6ca9c2a0efb190a6ddb99b044858cbca1be4
Andrey Pakhomov <pahomeathome@gmail.com>
2022-07-01 20:39:27 +0300

    main files added

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as api from './js/api';
+import * as headerHome from './js/header-home';
+import * as headerLibrary from './js/header-library';
+import * as card from './js/card';
+import * as modal from './js/modal';
---
+++ src/js/api.js
@@ -0,0 +1,1 @@
+const API_KEY = `1b50ba0e0b99203af5e26bdcee6d2298`;
