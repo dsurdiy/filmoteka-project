f388a22412811383f00b630781c05dd39f91ee18
Andrey Pakhomov <pahomeathome@gmail.com>
2022-07-05 23:52:34 +0300

    different indexes for home and library added

--- src/index-library.js
+++ src/index-library.js
@@ -1,9 +1,5 @@
 import * as api from './js/api';
-import * as headerHome from './js/header-home';
 import * as headerLibrary from './js/header-library';
 import * as card from './js/card';
 import * as modal from './js/modal';
-import * as popular from './js/popular';
 import * as loader from './js/loader';
-
-
