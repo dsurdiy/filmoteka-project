01fdd8bacdd90253da5e6ed911bdf2df97d65dc0
Slava Grobovoy <slava.grobovoy27@ukr.net>
2022-07-06 12:48:21 +0300

    added the opening of a modal window
    also added an id to the rendering of popular movies

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,62 +1,85 @@
+import axios from 'axios'
 
 const refs = {
-   btnClick: document.querySelector('.btn'),
+   cardsWrapper: document.querySelector('.popular__mov'),
    modal: document.querySelector('.film-details'),
    closeBtn: document.querySelector('.modal__icon-close'),
    modalWrapper: document.querySelector('.modal-wrapper'),
    backdrop: document.querySelector('.backdrop'),
    filmDetails: document.querySelector('.film-details'),
 }
+const API_KEY = '1b50ba0e0b99203af5e26bdcee6d2298'
 
-refs.btnClick.addEventListener('click', renderModal)
+refs.cardsWrapper.addEventListener('click', onMovieClick)
+
+  
+async function getDetails(id) {
+
+  try {
+    return await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
+    .then( response => {
+      return response.data
+    })
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 
-
-function modalMarkup() {
+async function onMovieClick(e) {
+  const movieCard = e.target.closest('.movie');
+  const movieId = movieCard.dataset.movieid;
+  console.log(movieId);
+  const response = await getDetails(movieId)
+  console.log(response);
+  renderModal()
+  refs.filmDetails.insertAdjacentHTML('beforeend', modalMarkup(response)) 
+  
+}
+  
+function modalMarkup(res) {
+  console.log(res);
+    
+   const markup = `
    
-   
-    return `
       <img
         class="film-details__image"
-        src="https://fakeimg.pl/350x200/?text=Hello"
-        alt="временная картинка"
+        src="https://image.tmdb.org/t/p/w500${
+     res.poster_path
+   }"
+        alt="${res.title}"
       />
       <div class="film-details__wrapper">
-        <h2 class="film-details__title">Title</h2>
+        <h2 class="film-details__title">${res.title}</h2>
         <ul class="film-details__list ">
           <li class="film-details__item">
             <p class="film-details__text">Vote / Votes</p>
             <span class="film-details__span film-details__span--accent"
-              >7.3</span
+              >${res.vote_average}</span
             >
             <span class="film-details__separator">/</span>
             <span class="film-details__span film-details__span--noaccent"
-              >1260</span
+              >${res.vote_count}</span
             >
           </li>
           <li class="film-details__item">
             <p class="film-details__text">Popularity</p>
-            <span class="film-details__span">100.2</span>
+            <span class="film-details__span">${res.popularity.toFixed(2)}</span>
           </li>
           <li class="film-details__item">
             <p class="film-details__text">Original Title</p>
-            <span class="film-details__span">A FISTFUL OF LEAD</span>
+            <span class="film-details__span">${res.original_title}</span>
           </li>
           <li class="film-details__item">
             <p class="film-details__text">Genre</p>
-            <span class="film-details__span">Comedia</span>
+            <span class="film-details__span">${res.genres
+                .map(genre => genre.name).slice(0, 2).join(', ')}</span>
           </li>
         </ul>
         <p class="film-details__about">About</p>
         <p class="film-details__overview">
-          Four of the West’s most infamous outlaws assemble to steal a huge
-          stash of gold from the most corrupt settlement of the gold rush towns.
-          But not all goes to plan one is killed and the other three escapes
-          with bags of gold hide out in the abandoned gold mine where they
-          happen across another gang of three – who themselves were planning to
-          hit the very same bank! As tensions rise, things go from bad to worse
-          as they realise the bags of gold are filled with lead... they’ve been
-          double crossed – but by who and how?
+          ${res.overview}
         </p>
 
        
@@ -72,9 +95,13 @@ function modalMarkup() {
       </div>
    
    
-  `
-   
+  `  
+  return markup
 }
+
+
+
+
 
 function closeModalBackdrop(e) {
 
@@ -105,9 +132,18 @@ function toggleModal() {
    
 }
 function renderModal() {
-   refs.filmDetails.textContent = ''
-   refs.modalWrapper.classList.remove('is-hidden');
-   refs.filmDetails.insertAdjacentHTML('beforeend', modalMarkup()) 
-   toggleModal()
+  refs.filmDetails.textContent = ''
+  refs.modalWrapper.classList.remove('is-hidden');
+  
+  
+  toggleModal()
+  
 }
+
+
+
+
+
+
+
 
