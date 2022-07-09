7faab5f133e9e4a8fd53ee79b9a25495c2b99e65
ZABILA PETRO <petrozabila@gmail.com>
2022-07-10 00:03:42 +0300

    url changed

--- src/js/popular.js
+++ src/js/popular.js
@@ -2,11 +2,10 @@
 
 const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY+'&genre';
+const API_URL = BASE_URL + '/trending/movie/day?'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 const main = document.querySelector('.popular__mov');
-
 const genres = [
     {
       "id": 28,
@@ -96,7 +95,7 @@ async function getMovies(url) {
     try {
         const response = await fetch(url);
         const toJson = await response.json();
-       
+       console.log(toJson.total_pages);
         showMovies(toJson.results);
       }
     
@@ -107,7 +106,7 @@ async function getMovies(url) {
 
 function showMovies(data) {
 
-    const markup = data.map(({title, poster_path, release_date, genre_ids}) => {
+    const markup = data.map(({title, poster_path, release_date, genre_ids, id}) => {
        let movieGenre;
 
         movieGenre = genres.filter(el => el.id === genre_ids[0] || el.id === genre_ids[1] || el.id === genre_ids[2])
@@ -123,9 +122,79 @@ function showMovies(data) {
                     <div class="movie__description"> 
                         <div class="movie__genre">${movieGenre}</div>
                         <div class="movie__release_date">${year}</div>
+                
                     </div>
                 </div>
         </div>`
     }).join("");
     main.insertAdjacentHTML('beforeend', markup);
 }
+
+
+// let page = 1;
+
+// const link = document.querySelector('.number');
+// const wrapper = document.querySelector('.popular__mov');
+// const previuos = document.querySelector('.previous');
+// const next = document.querySelector('.next');
+// const navigation = document.querySelector('.navigation');
+
+
+// document.addEventListener('click', handleLink);
+// previuos.addEventListener('click', handlePrevBtn);
+// next.addEventListener('click', handleNextBtn);
+
+// function handleLink(e) {
+
+//   if(e.target.className === "number") {
+//     console.log(e.target.className);
+//     page = e.target.innerText;
+//     console.log(page);
+//     wrapper.innerHTML = "";
+//     document.removeEventListener("DOMContentLoaded", popularOnPageLoad);
+//     getMovies(BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY+`&genre&page=${page}`);
+//   }
+// }
+
+// function handlePrevBtn(e) {
+//   if(Number(page) === 1) {
+//     return
+//   } else {
+//     console.log(page);
+//     page = Number(page) - 1;
+//     wrapper.innerHTML = "";
+//     document.removeEventListener("DOMContentLoaded", popularOnPageLoad);
+//     getMovies(BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY+`&genre&page=${page}`);
+//   }
+//   if(page > 1) {
+     
+//     const addPage = `<button type="button" class="number">${Number(page) - 1}</button>
+//     <button type="button" class="number">${Number(page)}</button>
+//     <button type="button" class="number">${Number(page) + 1}</button>
+//     `
+//     navigation.innerHTML = ""
+
+//     navigation.insertAdjacentHTML('beforeend', addPage)
+//   }
+// }
+
+// function handleNextBtn() {
+//     page = Number(page) + 1;
+//     console.log(page);
+//     wrapper.innerHTML = "";
+//     document.removeEventListener("DOMContentLoaded", popularOnPageLoad);
+//     getMovies(BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY+`&genre&page=${page}`);
+
+//     if(page > 1) {
+     
+//       const addPage = `<button type="button" class="number">${Number(page) - 1}</button>
+//       <button type="button" class="number">${Number(page)}</button>
+//       <button type="button" class="number">${Number(page) + 1}</button>
+//       `
+//       navigation.innerHTML = ""
+
+//       navigation.insertAdjacentHTML('beforeend', addPage)
+//     }
+// }
+
+
