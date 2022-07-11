44c396986f02b2166521d3194619a974c9addd56
Andrey Pakhomov <pahomeathome@gmail.com>
2022-07-11 21:37:45 +0300

    pagination added for search

--- src/js/movie-service.js
+++ src/js/movie-service.js
@@ -1,44 +1,32 @@
 const axios = require('axios');
 const API_KEY = `1b50ba0e0b99203af5e26bdcee6d2298`;
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie'
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
 
 export default class NewsApiService {
-    constructor() {
-        this.searchQuery = '';
-        this.page = 1;
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchThemovie() {
+    const url = `${BASE_URL}?api_key=${API_KEY}&language=en-US&page=${this.page}&include_adult=false&query=${this.searchQuery}`;
+
+    try {
+      const axiosFetch = await axios.get(url);
+
+      const { data } = axiosFetch;
+
+      return data.results;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async fetchThemovie() {
+  get query() {
+    return this.searchQuery;
+  }
 
-        const url = `${BASE_URL}?api_key=${API_KEY}&language=en-US&page=${this.page}&include_adult=false&query=${this.searchQuery}`;
-
-        try {
-            const axiosFetch = await axios.get(url);
-         
-            const { data } = axiosFetch;
-
-            this.incrementPage();
-
-            return data.results;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    incrementPage() {
-        this.page += 1;
-    }
-
-    resetPage() {
-        this.page = 1;
-    }
-
-    get query() {
-        return this.searchQuery;
-    }
-
-    set query(newQuery) {
-        this.searchQuery = newQuery
-    }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
-
