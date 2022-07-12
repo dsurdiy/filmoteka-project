9b050102aeff93af2487ae594d7732a246b7b89f
Andrey Pakhomov <pahomeathome@gmail.com>
2022-07-12 18:21:50 +0300

    pagination added for library

--- src/js/pagination-library.js
+++ src/js/pagination-library.js
@@ -1,4 +1,8 @@
 import * as paginationjs from 'paginationjs';
+import { watched } from './showWatchedQueue';
+import { queue } from './showWatchedQueue';
+
+export let page = 1;
 
 let previousPagesShow = false;
 
@@ -13,12 +17,46 @@ watchedBtn.addEventListener('click', makePagination);
 queueBtn.addEventListener('click', makePagination);
 
 function makePagination(event) {
+  page = 1;
   document.querySelector('#pagination-container').innerHTML = '';
   const action = event.target.dataset.action;
   const check = JSON.parse(localStorage.getItem(action.charAt(0).toUpperCase() + action.slice(1)));
   if (check && check.length > 0) {
     $('#pagination-container').pagination({
       dataSource: check,
+      afterPageOnClick: function (event) {
+        event.preventDefault();
+        document.querySelector('.watched_and_queue').innerHTML = '';
+        page = parseInt(event.target.innerText);
+        if (action === 'watched') {
+          watched(event);
+        }
+        if (action === 'queue') {
+          queue(event);
+        }
+      },
+      afterPreviousOnClick: function (event) {
+        event.preventDefault();
+        document.querySelector('.watched_and_queue').innerHTML = '';
+        page -= 1;
+        if (action === 'watched') {
+          watched(event);
+        }
+        if (action === 'queue') {
+          queue(event);
+        }
+      },
+      afterNextOnClick: function (event) {
+        event.preventDefault();
+        document.querySelector('.watched_and_queue').innerHTML = '';
+        page += 1;
+        if (action === 'watched') {
+          watched(event);
+        }
+        if (action === 'queue') {
+          queue(event);
+        }
+      },
       pageSize: 20,
       prevText: '',
       nextText: '',
--- src/js/showWatchedQueue.js
+++ src/js/showWatchedQueue.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import alertMessage from './alert'
+import alertMessage from './alert';
 import cardTemplate from '../template/film-cardWQ.hbs';
-
 
 const API_KEY = 'api_key=1b50ba0e0b99203af5e26bdcee6d2298';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
@@ -14,27 +13,28 @@ watchedBtn.addEventListener('click', watched);
 queueBtn.addEventListener('click', queue);
 let watchedMoviesId = [];
 
-function watched() {
+import { page } from './pagination-library';
+
+export function watched(event) {
   clearQueue();
+
   watchedMoviesId = JSON.parse(localStorage.getItem('Watched'));
   if (watchedMoviesId === 0 || watchedMoviesId < 1) {
-   return alertMessage();
+    return alertMessage();
   }
-  
-  watchedMoviesId.map(id => {
+  watchedMoviesId.slice(page * 20 - 20, page * 20).map(id => {
     getWQMovies(id);
   });
 }
 
-function queue() {
+export function queue(event) {
   clearWatched();
   watchedMoviesId = JSON.parse(localStorage.getItem('Queue'));
 
   if (watchedMoviesId === 0 || watchedMoviesId < 1) {
-   return alertMessage();
+    return alertMessage();
   }
-
-  watchedMoviesId.map(id => {
+  watchedMoviesId.slice(page * 20 - 20, page * 20).map(id => {
     getWQMovies(id);
   });
 }
@@ -49,22 +49,22 @@ async function getWQMovies(id) {
   }
 }
 
- function showWatchedMovies(data) {
-   const { title, poster_path, release_date, genres, id, vote_average } = data
-   if (genres.length > 3) {
-      genres.length = 3;
-   }
-   let genre = genres.reverse().map(genre => {
-       return  " "+genre.name;
-   })
-   
-   let dateToYear = new Date(release_date);
-   const year = dateToYear.getFullYear();
-   
-   let vote = vote_average.toFixed(1);
-   
-   const markup = cardTemplate({ title, poster_path, year, movieGenre: genre, id,vote });
-   wqGallery.insertAdjacentHTML("beforeend", markup);
+function showWatchedMovies(data) {
+  const { title, poster_path, release_date, genres, id, vote_average } = data;
+  if (genres.length > 3) {
+    genres.length = 3;
+  }
+  let genre = genres.reverse().map(genre => {
+    return ' ' + genre.name;
+  });
+
+  let dateToYear = new Date(release_date);
+  const year = dateToYear.getFullYear();
+
+  let vote = vote_average.toFixed(1);
+
+  const markup = cardTemplate({ title, poster_path, year, movieGenre: genre, id, vote });
+  wqGallery.insertAdjacentHTML('beforeend', markup);
 }
 
 function clearWatched() {
