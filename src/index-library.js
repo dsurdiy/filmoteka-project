00945b275a4b1db62f62b966f414fa633ff27a05
Andrii Pakhomov <100218289+PaHomeAtHome@users.noreply.github.com>
2022-07-12 16:00:29 +0300

    Update index-library.js

--- src/index-library.js
+++ src/index-library.js
@@ -4,6 +4,5 @@ import * as loader from './js/loader';
 import * as showWatchedQueue from './js/showWatchedQueue';
 import * as pagination from './js/pagination-library';
 import { onScroll, onToTopBtn } from './js/scroll-to-top';
-onScroll();
-onToTopBtn();
-import * as footer from './js/footer-modal';
+onScroll()
+onToTopBtn()
