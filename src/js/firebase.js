d161076cecec873f3e0cfb6473dadb7707e68d46
Andrii Pakhomov <100218289+PaHomeAtHome@users.noreply.github.com>
2022-07-13 12:59:46 +0300

    Update firebase.js

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -125,6 +125,5 @@ btnSignup.addEventListener('click', createAccount);
 btnLogout.addEventListener('click', logout);
 
 const auth = getAuth(firebaseApp);
-connectAuthEmulator(auth, 'http://localhost:9099');
 
 monitorAuthState();
